a6be567faea0c070b39712cd04a3de7323619a5d
pcpasapas <98660822+pcpasapas@users.noreply.github.com>
2022-08-31 09:28:01 +0200

    modif router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   target: 'static',
+  router: {
+    base: '/pasapasnew/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Pc Pas a Pas',
