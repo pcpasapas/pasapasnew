80bab76e7c0d397be658dab8d8767049d338695c
pcpasapas <98660822+pcpasapas@users.noreply.github.com>
2022-09-01 12:23:49 +0200

    82397ff

---
+++ _nuxt/static/1661940738/tutosfront/29/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpasapasnew\u002F_nuxt\u002Fstatic\u002F1661940738",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Ftutosfront\u002F29",config:{_app:{basePath:"\u002Fpasapasnew\u002F",assetsPath:"\u002Fpasapasnew\u002F_nuxt\u002F",cdnURL:a}}}}(null));
