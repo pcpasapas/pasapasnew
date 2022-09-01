5ecce6348c97d01e26d049489203abaf36a7e59e
pcpasapas <98660822+pcpasapas@users.noreply.github.com>
2022-09-01 16:28:50 +0200

    aaa

---
+++ dist/_nuxt/static/1662042398/tutosfront/6/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662042398",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Ftutosfront\u002F6",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <NavbarComponent />
     <h1>Découvrir les tutos Nuxt</h1>
+    <h2>Modifié le 01/09</h2>
   </div>
 
 </template>
