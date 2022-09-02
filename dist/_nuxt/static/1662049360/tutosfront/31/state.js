b27a4bd3c9573bbe9cbf831e3e43f78eea2738ca
pcpasapas <98660822+pcpasapas@users.noreply.github.com>
2022-09-02 08:00:40 +0200

    aaa

--- dist/_nuxt/static/1662049360/tutosfront/31/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662049360",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Ftutosfront\u002F31",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
